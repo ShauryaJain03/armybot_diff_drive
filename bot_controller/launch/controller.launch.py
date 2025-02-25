730d9ebf1c4a309505acce801952e4988d7e4b0c
Shaurya Jain <jainshaurya.sj@gmail.com>
2025-02-25 14:36:22 +0530

    qr following working

--- bot_controller/launch/controller.launch.py
+++ bot_controller/launch/controller.launch.py
@@ -15,18 +15,16 @@ def generate_launch_description():
         ],
     )
 
-    bot_controller = Node(
-        package="controller_manager",
-        executable="spawner",
-        arguments=["bot_controller", 
-                   "--controller-manager", 
-                   "/controller_manager"
-        ],
+    diff_drive_controller_spawner = Node(
+        package='controller_manager',
+        executable='spawner',
+        arguments=['bot_controller', '--controller-manager', '/controller_manager'],
+        output='screen'
     )
 
     return LaunchDescription(
         [
             joint_state_broadcaster_spawner,
-            bot_controller
+            diff_drive_controller_spawner
         ]
     )
