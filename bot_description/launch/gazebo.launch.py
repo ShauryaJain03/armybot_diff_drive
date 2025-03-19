02bbe5c5337165b9a446db21b2e5a905c0139389
Shaurya Jain <jainshaurya.sj@gmail.com>
2025-03-19 22:41:47 +0530

    new worlds configured

--- bot_description/launch/gazebo.launch.py
+++ bot_description/launch/gazebo.launch.py
@@ -1,10 +1,11 @@
 import os
+from os import pathsep
 from pathlib import Path
 from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument, IncludeLaunchDescription, SetEnvironmentVariable
-from launch.substitutions import Command, LaunchConfiguration
+from launch.substitutions import Command, LaunchConfiguration,PathJoinSubstitution, PythonExpression
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from launch_ros.actions import Node
@@ -20,12 +21,14 @@ def generate_launch_description():
                                       description="Absolute path to robot urdf file"
     )
 
+    model_path = str(Path(bot_description).parent.resolve())
+    model_path += pathsep + os.path.join(get_package_share_directory("bot_description"), 'models')
+
     gazebo_resource_path = SetEnvironmentVariable(
-        name="GZ_SIM_RESOURCE_PATH",
-        value=[
-            str(Path(bot_description).parent.resolve())
-            ]
+        "GZ_SIM_RESOURCE_PATH",
+        model_path
         )
+
     
     robot_description = ParameterValue(Command([
             "xacro ",
@@ -42,6 +45,14 @@ def generate_launch_description():
                      "use_sim_time": True}]
     )
 
+    world_name_arg = DeclareLaunchArgument(name="world_name", default_value="empty")
+
+    world_path = PathJoinSubstitution([
+            bot_description,
+            "worlds",
+            PythonExpression(expression=["'", LaunchConfiguration("world_name"), "'", " + '.world'"])
+        ]
+    )
 
     joint_state_pub = Node(
             package='joint_state_publisher',
@@ -54,11 +65,11 @@ def generate_launch_description():
     gazebo = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
                     get_package_share_directory("ros_gz_sim"), "launch"), "/gz_sim.launch.py"]),
-                launch_arguments=[
-                    ("gz_args", [" -v 4", " -r", " empty.sdf"]
-                    )
-                ]
-             )
+                launch_arguments={
+                    "gz_args": PythonExpression(["'", world_path, " -v 4 -r'"])
+                }.items()
+             
+    )
 
     gz_spawn_entity = Node(
         package="ros_gz_sim",
@@ -94,6 +105,7 @@ def generate_launch_description():
         robot_state_publisher_node,
         gazebo,
         gz_spawn_entity,
+        world_name_arg,
         ros_gz_image_bridge,
         gz_ros2_bridge,
         joint_state_pub
